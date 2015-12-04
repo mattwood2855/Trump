4e4b377a542ba759b535334f808e7f505f8ae0db
treighton <treighton@gmail.com>
2015-12-03 16:30:19 -0800

    updated app

--- js/app.js
+++ js/app.js
@@ -5,13 +5,9 @@ winW = document.body.offsetWidth;
 winH = document.body.offsetHeight;
 var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'Trump');
 
-<<<<<<< Updated upstream
-//game.state.add('Menu', Menu, true);
-game.state.add('Game', PhaserGame, true);
-=======
 game.state.add('Menu', Menu, true);
 
 game.state.add('Game', PhaserGame, true);
 
 game.state.start('Menu');
->>>>>>> Stashed changes
+
